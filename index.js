148ded9413c4c77451957748d2556d2e19fe4e96
ananthvishnu <vishnurathan834@gmail.com>
2023-02-20 20:02:23 +0530

    digital clock added

---
+++ index.js
@@ -0,0 +1,29 @@
+const clock=document.querySelector('.clock');
+
+function runClock(){
+    var time=new Date();
+    var hrs=time.getHours();
+    var min=time.getMinutes();
+    var sec=time.getSeconds();
+    var txt="AM"
+
+    if(hrs>12){
+        hrs=hrs-12;
+        txt="PM";
+    }
+    else if(hrs==0){
+        hrs=12;
+        txt="AM";
+    }
+
+    hrs=hrs<10?'0'+hrs:hrs;
+    min=min<10?'0'+min:min;
+    sec=sec<10?'0'+sec:sec;
+
+
+    clock.innerHTML=(`${hrs}:${min}:${sec}:${txt}`);
+    
+}
+runClock();
+
+setInterval(runClock,1000);
